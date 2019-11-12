OCA/delivery-carrier

[MIG] port delivery_carrier_label_default to V10

delivery_carrier_label_default/__manifest__.py
@@ -1,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Sunflower IT (http://sunflowerweb.nl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{'name': 'Default label for carrier labels',
+ 'version': '10.0.1.0.0',
+ 'author': "Sunflower IT,Camptocamp,Odoo Community Association (OCA)",
+ 'maintainer': 'Sunflower IT',
+ 'category': 'Delivery',
+ 'depends': [
+     'base_delivery_carrier_label',
+ ],
+ 'website': 'http://sunflowerweb.nl',
+ 'data': [
+     'report/report_default_label.xml',
+     'report/report_paper_format.xml',
+ ],
+ 'installable': True,
+ 'license': 'AGPL-3'
+}

moylop260/carrier-delivery

delivery_carrier_label_default/__manifest__.py
@@ -1,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016 Sunflower IT (http://sunflowerweb.nl)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{'name': 'Default label for carrier labels',
+ 'version': '10.0.1.0.0',
+ 'author': "Sunflower IT,Camptocamp,Odoo Community Association (OCA)",
+ 'maintainer': 'Sunflower IT',
+ 'category': 'Delivery',
+ 'depends': [
+     'base_delivery_carrier_label',
+ ],
+ 'website': 'http://sunflowerweb.nl',
+ 'data': [
+     'reports/report_default_label.xml',
+     'reports/report_paper_format.xml',
+ ],
+ 'installable': True,
+ 'license': 'AGPL-3', }